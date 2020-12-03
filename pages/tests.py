a034c6dc01b9b4e0342484d046026a96dd011eee
darkbot <darkbotbbl@gmail.com>
2020-12-03 07:59:02 +0000

    Added a test to check for the particular template that is used by the HomePageView

--- pages/tests.py
+++ pages/tests.py
@@ -4,12 +4,15 @@ from django.urls import reverse
 
 class HomePageViewTests(SimpleTestCase):
 
-    # test the response code of the homepage route
     def test_homepageview_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    # test the response code of the reverse of the homepage route
     def test_homepageview_urlname_status_code(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
+ 
+    def test_homepageview_template_used(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
