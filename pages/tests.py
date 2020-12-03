db168746c450520a904c1f897f8ff9990f81ebdb
darkbot <darkbotbbl@gmail.com>
2020-12-03 07:51:06 +0000

    Added two tests to check for the status code of the url routing

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageViewTests(SimpleTestCase):
+
+    # test the response code of the homepage route
+    def test_homepageview_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    # test the response code of the reverse of the homepage route
+    def test_homepageview_urlname_status_code(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
