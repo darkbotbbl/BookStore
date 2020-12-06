50522fa8dcaf063a3b3baa52ce8746ac286eb5f6
darkbot <darkbotbbl@gmail.com>
2020-12-06 07:51:06 +0000

    Instaled django-allauth to manage more advanced user creation

--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,8 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = "pages/home.html"
+	template_name = "pages/home.html"
 
 
 class AboutPageView(TemplateView):
-    template_name = "pages/about.html"
+	template_name = "pages/about.html"
