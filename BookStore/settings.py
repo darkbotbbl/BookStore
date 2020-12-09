9ada1de48a2ea61f12ceb06e3039b4177ff46e08
darkbot <darkbotbbl@gmail.com>
2020-12-09 13:17:23 +0000

    Moved the secrete key to an environment variable

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%uu7x3q=1b^q!zu)nan3g4-_sitsa#=htrog2vqa=+)@daetg2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
