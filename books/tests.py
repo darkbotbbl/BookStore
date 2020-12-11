5757190d8a5a22a5f386b401527f15c178f019f5
darkbot <darkbotbbl@gmail.com>
2020-12-11 16:07:50 +0000

    Commited book tests.py

--- books/tests.py
+++ books/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from .models import Book
 
 
-class BookModelTests(TestCase):
+class BookTests(TestCase):
 
     def test_book_object_creation(self):
         book = Book.objects.create(
@@ -13,3 +13,5 @@ class BookModelTests(TestCase):
         self.assertEqual(book.title, "The first book")
         self.assertEqual(book.author, "Malor Kaduna")
         self.assertEqual(book.price, 520.22)
+
+    # TODO - add tests for the urls, templates, and views
