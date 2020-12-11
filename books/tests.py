7544d4acb715af97b72c57aa4d576c425b546217
darkbot <darkbotbbl@gmail.com>
2020-12-11 09:15:10 +0000

    wrote test for creation of a Book instance

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+
+class BookModelTests(TestCase):
+
+    def test_book_object_creation(self):
+        book = Book.objects.create(
+            title="The first book",
+            author="Malor Kaduna",
+            price=520.22,
+        )
+        self.assertEqual(book.title, "The first book")
+        self.assertEqual(book.author, "Malor Kaduna")
+        self.assertEqual(book.price, 520.22)
